317e99eacf7e1b5b4f9617d29b0f6f2000fc0c79
Korwai <ineedassistancetakingajoke@gmail.com>
2020-02-01 12:37:27 -0800

    update

--- LAcademy/src/main/java/Main.java
+++ LAcademy/src/main/java/Main.java
@@ -10,15 +10,28 @@ import com.google.cloud.firestore.WriteResult;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        String path = "C:\\Users\\Alex\\Desktop\\LAcademy-11261bcdb733.json";
         Instance serviceAccount;
         ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        serviceAccount = mapper.readValue(new File("C:\\Users\\Alex\\Desktop\\LAcademy-11261bcdb733.json"), Instance.class);
+        serviceAccount = mapper.readValue(new File("C:\\Users\\Alex\\Desktop\\lacademy-e1e8a-firebase-adminsdk-ybrvw-1f4a96ef88.json"), Instance.class);
 
-        // System.out.println(serviceAccount.getProject_id());
+        QuickStart.authExplicit("C:\\Users\\Alex\\Desktop\\lacademy-e1e8a-firebase-adminsdk-ybrvw-1f4a96ef88.json");
         QuickStart qs = new QuickStart(serviceAccount.getProject_id());
+
+
+        // Stuff to test adding stuff to database.
+        Map<String, String> doc = new HashMap<String, String>();
+        doc.put("name", "Los Angeles");
+        doc.put("state", "CA");
+        doc.put("country", "USA");
+        ApiFuture<WriteResult> future = qs.getDb().collection("cities").document("LA").set(doc);
+        System.out.println("Update time: " + future.get().getUpdateTime());
 
 //
 //        FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder()
